c41eb5cf87089fedaee668c645d063fa5cf16a3b
Gibigabcsusz <gbr.szilagyi@gmail.com>
2022-12-28 15:55:26 +0100

    co mparison generator.c programka

---
+++ comparison_generator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n=32;
+    for(int p=1; p<n; p*=2)
+    {
+        for(int k=p; k>=1; k/=2)
+        {
+            for(int j=k%p; j<=n-k-1; j+=2*k)
+            {
+                for(int i=0; i<k; i++)
+                {
+                    if((i+j)/(p*2)==(i+j+k)/(p*2) && i+j+k<25)
+                        //a<b ? {} : {tmp=a; a=b; b=tmp;};
+                        printf("values[%2d]<values[%2d] ? {} : {tmp=values[%2d]; values[%2d]=values[%2d]; values[%2d]=tmp;};\n", i+j, i+j+k, i+j, i+j, i+j+k, i+j+k);
+                }
+            }
+        }
+    }
+    return 0;
+}
